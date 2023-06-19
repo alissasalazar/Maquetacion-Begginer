c5ecc87f8ef4604739eadfbbbb6be28a486b39ff
alissasalazar <salazargamarraa@gmail.com>
2023-06-19 16:08:15 -0500

    fix: code about footer and portfolio

--- Components/Footer/Footer.jsx
+++ Components/Footer/Footer.jsx
@@ -1,26 +1,28 @@
-import "../Footer/footer.css"
-
+import "../Footer/footer.css";
 
 export function Footer() {
   return (
     <div className="contentFooter">
-      <div className="contentAllMessage">
-        <h2 className="titleMessage">LOCATION</h2>
-        <p className="contentMessage">3481 Melrose Place Beverly Hills, CA 90210</p>
-      </div>
-      <div className="contentAllMessage">
-        <h2 className="titleMessage">AROUND THE WEB</h2>
-        <div>
-
+      <div className="contentFooterDescriptions">
+        <div className="contentAllMessage">
+          <h2 className="titleMessage">LOCATION</h2>
+          <p className="contentMessage">
+            3481 Melrose Place Beverly Hills, CA 90210
+          </p>
+        </div>
+        <div className="contentAllMessage">
+          <h2 className="titleMessage">AROUND THE WEB</h2>
+          <div></div>
+        </div>
+        <div className="contentAllMessage">
+          <h1 className="titleMessage">ABOUT FREELANCER</h1>
+          <p className="contentMessage">
+            Freelance is a free to use , open source Bootstrap theme created by
+            <span className="specialMessage"> Start Bootstrap</span>
+          </p>
         </div>
       </div>
-      <div className="contentAllMessage">
-        <h1 className="titleMessage">ABOUT FREELANCER</h1>
-        <p className="contentMessage">
-          Freelance is a free to use , open source Bootstrap theme created by <p className="specialMessage"> Start Bootstrap</p>
-        </p>
-      </div>
-      <p className="copyRigthMessage">Copyright Your Website 2017</p>
+      <p  className="copyRigthMessage">Copyright© Your Website 2017</p>
     </div>
   );
 }
